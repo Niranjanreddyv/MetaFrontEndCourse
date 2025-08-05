4e706a7089a0de2fba885fe0de807c155ab3c426
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-05 23:38:13 +0530

    Boolean in Js add

---
+++ JavaScript/Boolean.js
@@ -0,0 +1,13 @@
+console.log(1<2);
+console.log(1>2);
+console.log(1 == 2);
+console.log(1 == 1);
+console.log(1 != 1);
+console.log(1 != 2);
+console.log(1 == '1');
+console.log(1 != '1');
+console.log(1 === 1);
+console.log(1 === '1');
+console.log(1 !== '1'); // strict equality 
+console.log(1 !== 1);
+// both check the type and == do type conversion === not do.
