8c0c9f7f59c9679b2973bf536e7c5dade0c10a28
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-05 23:18:55 +0530

    Number in js add

---
+++ Number.js
@@ -0,0 +1,16 @@
+console.log(123);
+console.log(typeof(123));
+console.log(123.45);
+console.log(typeof(123.45));
+console.log(4 + 2);
+console.log(4 - 2);
+console.log(4 * 2);
+console.log(4 / 2);
+console.log(4 > 2);
+console.log(4 < 2);
+console.log( 4 == 2 );
+console.log(4**2); // power
+console.log(9 % 8); // remainder
+console.log(16 % 8);
+console.log( 2*4+8);
+console.log(2 * (4+8));
