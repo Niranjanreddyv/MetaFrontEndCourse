0bbb034c37717d6997d282814277e29af8158dea
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-05 23:27:41 +0530

    String all add

---
+++ JavaScript/Strings.js
@@ -0,0 +1,7 @@
+console.log('hi google');
+console.log("hi google");
+console.log(`hi google`);
+console.log('It "is a game" to play');
+console.log("It 'is a game' to play");
+console.log("It's Raining.");
+console.log(`It's Rainging`);
