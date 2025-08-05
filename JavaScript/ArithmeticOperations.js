290e459c10589f0e29b94fb8673dfc027bd9f1be
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-05 22:51:47 +0530

    Arithematic & Comparision Operaters add

---
+++ JavaScript/ArithmeticOperations.js
@@ -0,0 +1,25 @@
+/*
+
+    Arithematic Operations in JS
+    + Addition
+    - Substration
+    * Multiplication
+    / dividion
+*/
+
+/*
+
+    Comparision Operatores in JS
+    > greater than
+    < less than
+    == equal to
+
+*/ 
+
+console.log(2 + 3);
+console.log(4 - 2);
+console.log(2 * 3);
+console.log(3 / 2);
+console.log(3>4);
+console.log(3<4);
+console.log(10 == 10);
