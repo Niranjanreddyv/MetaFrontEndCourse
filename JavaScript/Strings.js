127618b9a71a1207afaf616575078e825b51706e
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-06 23:07:27 +0530

    strings update

--- JavaScript/Strings.js
+++ JavaScript/Strings.js
@@ -5,3 +5,8 @@ console.log('It "is a game" to play');
 console.log("It 'is a game' to play");
 console.log("It's Raining.");
 console.log(`It's Rainging`);
+
+var s1 = 'ram';
+var s2 = 'raju';
+console.log(s1 + s2);
+console.log(s1);
