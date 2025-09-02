4a85b992bf7f55886461aed5f07e8f613d142ab2
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-09-02 16:30:07 +0530

    var let const scope code add

---
+++ JavaScript/Advance/Scope.js
@@ -0,0 +1,16 @@
+// var keyword
+console.log(user);
+var user = "rama";
+console.log(user);
+var user = "raju";
+console.log(user);
+
+// let 
+let name1 = "sam";
+console.log(name1);
+// let name = 'jhone'; // gave error syntax error
+name1 = "sai";
+console.log(name1);
+
+const PI = 3.14;
+console.log(PI);
